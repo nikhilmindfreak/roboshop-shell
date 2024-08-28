c4ed0a083bf0bf8e8c476441a2925fc2c68dc227
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-08-28 19:04:44 -0400

    added

--- dispatch.sh
+++ dispatch.sh
@@ -39,3 +39,18 @@ mkdir /app
 curl -L -o /tmp/dispatch.zip https://roboshop-builds.s3.amazonaws.com/dispatch.zip
 cd /app 
 unzip /tmp/dispatch.zip
+
+cd /app 
+go mod init dispatch
+go get 
+go build
+
+
+
+systemctl daemon-reload
+
+
+systemctl enable dispatch 
+systemctl start dispatch
+
+
