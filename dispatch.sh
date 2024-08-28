6ae517e379c8b44b413597c954f8ecea2a90fa6d
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-08-28 18:58:51 -0400

    added

--- dispatch.sh
+++ dispatch.sh
@@ -9,3 +9,23 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes
+else
+    echo "You are super user."
+fi
+
+
+dnf install golang -y
