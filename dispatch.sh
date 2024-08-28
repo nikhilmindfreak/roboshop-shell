c1685f097298a06250256457e76b7db0fb1b2d9f
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-08-28 18:57:20 -0400

    added

---
+++ dispatch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
